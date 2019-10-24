d2bd248879ff65bac187846f99c2f795f384d87b
ThomasNGit <neronithomas1989@gmail.com>
2019-10-24 13:56:27 +0200

    creazione dell'array di 100 numeri

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,3 +3,20 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+//creazione dell'array che verrà stampato
+var array100 = new Array(100);
+
+//popolo l'array con 100 elementi
+for (var i = 0; i < 100; i++) {
+
+    array100[i] = i + 1; 
+
+}
+
+//stampo l'array 
+for (var j = 0; j < array100.length; j++) {
+
+    console.log(array100[j]); 
+
+}
