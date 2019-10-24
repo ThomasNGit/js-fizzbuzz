1cbd30693c42df6b61ee6dbda8e2a0ae26ab435e
ThomasNGit <neronithomas1989@gmail.com>
2019-10-24 14:25:05 +0200

    stampato in pagina con una lista ordinata la lista dei numeri e dei "fizz", "buzz", "fizzbuzz"

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,27 +14,45 @@ for (var i = 0; i < 101; i++) {
 
 }
 
+
 //stampo l'array 
 for (var j = 1; j < array100.length; j++) {
 
+    var numeroPrecedente = document.getElementById("numbers").innerHTML;
+
+    console.log(array100[j]);
+
+    document.getElementById("numbers").innerHTML = numeroPrecedente + "<li>" + array100[j] + "</li>";
+    
+
     if (j % 15 === 0) {
 
-        console.log("FizzBuzz");
+
+
+         document.getElementById("numbers").innerHTML = numeroPrecedente +  '<li>"FizzBuzz"</li>'
+
+         console.log("FizzBuzz");
         
 
-    } else if (j % 3 == 0) {
+     } else if (j % 3 == 0) {
 
-        console.log("Fizz");
-        
-    } else if (j % 5 == 0) {
+         document.getElementById("numbers").innerHTML = numeroPrecedente +  '<li>"Fizz"</li>'
 
-        console.log("Buzz");
+         console.log("Fizz");
         
-    } else {
+     } else if (j % 5 == 0) {
 
-        console.log(array100[j]);
+         document.getElementById("numbers").innerHTML = numeroPrecedente +   '<li>"Buzz"</li>'
+
+         console.log("Buzz");
+     }    
+    //  } else {
+
+    //      document.getElementById("numbers").innerHTML = numeroPrecedente +  array100[j];
+
+    //      console.log(array100[j]);
         
-    }
+    //  }
 
     
 }
